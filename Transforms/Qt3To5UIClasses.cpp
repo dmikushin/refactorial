c865135a26fc901b450b10c3d5b954034e8e962a
Ryan Wersal <ryan@ryanwersal.com>
2016-06-24 11:33:12 -0500

    Add further transform functionality for Qt3To5UIClasses
     * Rename of ctor initializers is in place
     * Add setupUi(this); line to UI class ctors

--- Transforms/Qt3To5UIClasses.cpp
+++ Transforms/Qt3To5UIClasses.cpp
@@ -7,6 +7,17 @@ using namespace clang::ast_matchers;
 REGISTER_TRANSFORM(Qt3To5UIClasses);
 
 //==============================================================================
+std::string getParentClassName(const std::string& parentclassname)
+{
+	if (parentclassname == "scr") {
+		return "ZScreen";
+	} else if (parentclassname == "ui") {
+		return "QWidget";
+	}
+	return parentclassname;
+}
+
+//==============================================================================
 class BaseConstructorHandler : public MatchFinder::MatchCallback
 {
 public:
@@ -42,13 +53,7 @@ public:
 
 				std::string accessmodifier = matched[1].str();
 				std::string classname = matched[2].str();
-				std::string parentclassname = matched[3].str();
-
-				if (parentclassname == "scr") {
-					parentclassname = "ZScreen";
-				} else if (parentclassname == "ui") {
-					parentclassname = "QWidget";
-				}
+				std::string parentclassname = getParentClassName(matched[3].str());
 
 				std::string replacement_text = accessmodifier + " " + parentclassname + ", public Ui::" + classname;
 				Replacer::instance().replace(range, replacement_text, *src_manager);
@@ -63,14 +68,66 @@ private:
 };
 
 //==============================================================================
+class ConstructorInitializerHandler : public MatchFinder::MatchCallback
+{
+public:
+	ConstructorInitializerHandler(Transform* transform)
+		: _transform(transform),
+		  _ui_ctor_init_regex(llvm::Regex("[^_ ,]+_([^, ]+)"))
+	{}
+
+	virtual void run(const MatchFinder::MatchResult& result) {
+		if (const clang::CXXConstructorDecl* ctor = result.Nodes.getNodeAs<clang::CXXConstructorDecl>("ctor")) {
+			clang::SourceLocation loc = ctor->getLocation();
+			if (!_transform->canChangeLocation(loc)) {
+				return;
+			}
+
+			clang::SourceManager* src_manager = result.SourceManager;
+			for (clang::CXXCtorInitializer* init : ctor->inits()) {
+				// Only consider if written explicitly and it is for calling a base ctor.
+				if (!init->isWritten() || !init->isBaseInitializer()) {
+					continue;
+				}
+
+				clang::SourceRange range = init->getSourceRange();
+				range.setEnd(init->getLParenLoc().getLocWithOffset(-1));
+				llvm::StringRef src_text = clang::Lexer::getSourceText(clang::CharSourceRange::getTokenRange(range),
+																	   *src_manager, clang::LangOptions(), 0);
+
+				llvm::SmallVector<llvm::StringRef, 2> matched;
+				if (!_ui_ctor_init_regex.match(src_text, &matched)) {
+					continue;
+				}
+
+				std::string parentclassname = getParentClassName(matched[1]);
+				Replacer::instance().replace(range, parentclassname, *src_manager);
+			}
+
+			// Also add the setupUi call while we are in here.
+			clang::CompoundStmt* stmt = llvm::cast<clang::CompoundStmt>(ctor->getBody());
+			// TODO: Almost certainly need to handle indentation better here but should be good enough for now.
+			Replacer::instance().insert(stmt->getLBracLoc().getLocWithOffset(1), "\n\tsetupUi(this);\n", *src_manager);
+		}
+	}
+
+private:
+	Transform* _transform;
+	llvm::Regex _ui_ctor_init_regex;
+};
+
+//==============================================================================
 void Qt3To5UIClasses::HandleTranslationUnit(clang::ASTContext& c)
 {
 	init();
 
-	BaseConstructorHandler HandlerForBaseConstructor(this);
-
 	MatchFinder finder;
+
+	BaseConstructorHandler HandlerForBaseConstructor(this);
 	finder.addMatcher(cxxRecordDecl().bind("class_definition"), &HandlerForBaseConstructor);
+
+	ConstructorInitializerHandler HandlerForConstructorInitializer(this);
+	finder.addMatcher(cxxConstructorDecl(hasAnyConstructorInitializer(anything())).bind("ctor"), &HandlerForConstructorInitializer);
 
 	finder.matchAST(c);
 }
