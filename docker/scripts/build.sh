2e115b7778e79f9b9ba5363ef082af12909f0aec
Dmitry Mikushin <dmitry@kernelgen.org>
2023-01-19 23:17:31 +0000

    Using mold, which makes linking a lot faster

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -2,5 +2,8 @@ set -e -x
 cd /project
 mkdir -p build-docker
 cd build-docker
-cmake -DCMAKE_BUILD_TYPE=Debug -DLLVM_DIR=/usr/local/lib/cmake/llvm -DClang_DIR=/usr/local/lib/cmake/clang -DCMAKE_CXX_COMPILER=g++-11 -G Ninja .. && \
+cmake -DCMAKE_BUILD_TYPE=Debug \
+	-DLLVM_DIR=/usr/local/lib/cmake/llvm -DClang_DIR=/usr/local/lib/cmake/clang \
+	-DCMAKE_CXX_COMPILER=g++-11 -DCMAKE_LINKER=mold -DCMAKE_CXX_FLAGS=-fuse-ld=mold \
+       	-G Ninja .. && \
 cmake --build .
